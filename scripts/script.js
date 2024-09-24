353827a638f8d8141e629bd362558e4260f44913
EllesNietes <xelleskreukx@gmail.com>
2024-09-24 22:19:33 +0200

    nav menu

--- scripts/script.js
+++ scripts/script.js
@@ -1,2 +1,22 @@
 // JavaScript Document
 console.log("hi");
+
+var menu = document.querySelector('header > button');
+
+menu.addEventListener('click', openMenu);
+
+function openMenu () {
+  var nav = document.querySelector('nav');
+  nav.classList.add('toonMenu');
+}
+
+var sluit =
+document.querySelector('nav button');
+
+sluit.addEventListener('click', sluitMenu);
+
+function sluitMenu () {
+  var nav = document.querySelector('nav');
+  nav.classList.remove('toonMenu');
+}
+
